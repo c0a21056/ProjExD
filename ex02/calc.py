a5345dd37278196944ab9c2ab7d75c1b806c4324
kondo <c0a210568e@edu.teu.ac.jp>
2022-06-21 15:11:56 +0900

    問題６

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,21 +10,21 @@ def button_click(event):
     tkm.showinfo(num, f"[{num}]ボタンが押されました")
     entry.insert(tk.END,f"{num}")
 
-#if __name__=="__main()__":
-root = tk.Tk()
-#root.geometry("300x500")
-root.title("電卓")
+if __name__ == "__main__":
+    root = tk.Tk()
+    #root.geometry("300x500")
+    root.title("電卓")
 
-for num in range(9,-1,-1): 
-    button = tk.Button(root, text=f"{num}", width=4, height=2,font=("Times New Roman", 30))
-    button.grid(row=r,column=c)
-    c+=1
-    button.bind("<1>",button_click)
-    if (num-1)%3 == 0:
-        r+=1
-        c=0
+    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]+["="]):
+        button = tk.Button(root, text=f"{num}", width=4, height=2,font=("Times New Roman", 30))
+        button.grid(row=r,column=c)
+        c+=1
+        button.bind("<1>",button_click)
+        if (i+1)%3 == 0:
+            r+=1
+            c=0
 
-entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
-entry.grid(row=0,columnspan=3)
+    entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
+    entry.grid(row=0,columnspan=3)
 
-root.mainloop()
+    root.mainloop()
