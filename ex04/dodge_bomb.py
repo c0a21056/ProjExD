d56df94fff0841fcf2ebef19d70fc17326205ed8
kondo <c0a210568e@edu.teu.ac.jp>
2022-07-05 17:06:31 +0900

    4限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,11 @@
 import pygame as pg
 import sys
 import random
+import tkinter.messagebox as tkm
+
+i=0
+c=0
+a=0
 
 def main():
     clock = pg.time.Clock()
@@ -11,7 +16,8 @@ def main():
     bgimg_rect = bgimg_sfc.get_rect() #Rect
     screen_sfc.blit(bgimg_sfc,bgimg_rect)
 
-    tori_sfc = pg.image.load("fig/6.png") #Surface
+    b = random.randint(0,9)
+    tori_sfc = pg.image.load(f"fig/{b}.png") #Surface
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rect = tori_sfc.get_rect() #Rect
     tori_rect.center = 900, 400
@@ -26,33 +32,67 @@ def main():
 
     
     while True:
+        global i,c,a
+
         screen_sfc.blit(bgimg_sfc,bgimg_rect)
 
         for event in pg.event.get():
             if event.type == pg.QUIT: return #xを押した場合の処理
 
         key_lit = pg.key.get_pressed() #こうかとんの移動
-        if key_lit[pg.K_UP] == True:
-            tori_rect.centery -=1
-        if key_lit[pg.K_DOWN] == True:
-            tori_rect.centery +=1
-        if key_lit[pg.K_LEFT] == True:
-            tori_rect.centerx -=1
-        if key_lit[pg.K_RIGHT] == True:
-            tori_rect.centerx +=1
+        if key_lit[pg.K_UP] == True: tori_rect.centery -=1
+        if key_lit[pg.K_DOWN] == True: tori_rect.centery +=1
+        if key_lit[pg.K_LEFT] == True: tori_rect.centerx -=1
+        if key_lit[pg.K_RIGHT] == True: tori_rect.centerx +=1
 
         if check_bound(tori_rect, screen_rect) != (1,1):
-            if key_lit[pg.K_UP] == True:
-                tori_rect.centery +=1
-            if key_lit[pg.K_DOWN] == True:
-                tori_rect.centery -=1
-            if key_lit[pg.K_LEFT] == True:
-                tori_rect.centerx +=1
-            if key_lit[pg.K_RIGHT] == True:
-                tori_rect.centerx -=1
+            if key_lit[pg.K_UP] == True: tori_rect.centery +=1
+            if key_lit[pg.K_DOWN] == True: tori_rect.centery -=1
+            if key_lit[pg.K_LEFT] == True: tori_rect.centerx +=1
+            if key_lit[pg.K_RIGHT] == True: tori_rect.centerx -=1
 
         if tori_rect.colliderect(bomb_rect) == True:
+            x =  tori_rect.centerx
+            y =  tori_rect.centery
+            b = random.randint(0,9)
+            tori_sfc = pg.image.load(f"fig/{b}.png")
+            tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+            tori_rect = tori_sfc.get_rect()
+            tori_rect.center =  x,y
+            vx *= -1
+            vy *= -1
+            i += 1
+        
+        if i >= 3:
+            tkm.showinfo("ゲームオーバー", "ゲームオーバーです")
             return pg.QUIT
+        
+        if key_lit[pg.K_u] == True:
+            vx *=1.02
+            vy *=1.02
+
+        if key_lit[pg.K_b] == True:
+            x =  bomb_rect.centerx
+            y =  bomb_rect.centery
+            a+=0.5
+            bomb_sfc = pg.Surface((20+a,20+a)) #Surface
+            bomb_sfc.set_colorkey((0,0,0))
+            pg.draw.circle(bomb_sfc, (255,0,0),(10+a/2,10+a/2),10+a/2)
+            bomb_rect = bomb_sfc.get_rect() #Rect
+            bomb_rect.centerx = x
+            bomb_rect.centery = y
+
+
+        if key_lit[pg.K_s] == True:
+            x =  bomb_rect.centerx
+            y =  bomb_rect.centery
+            a-=0.5
+            bomb_sfc = pg.Surface((20+a,20+a)) #Surface
+            bomb_sfc.set_colorkey((0,0,0))
+            pg.draw.circle(bomb_sfc, (255,0,0),(10+a/2,10+a/2),10+a/2)
+            bomb_rect = bomb_sfc.get_rect() #Rect
+            bomb_rect.centerx = x
+            bomb_rect.centery = y
 
         screen_sfc.blit(tori_sfc, tori_rect) 
 
@@ -61,8 +101,8 @@ def main():
         screen_sfc.blit(bomb_sfc, bomb_rect)
 
         yoko,tate =  check_bound(bomb_rect, screen_rect)
-        vx *= yoko
-        vy *= tate
+        vx *= yoko 
+        vy *= tate 
 
         pg.display.update()
         clock.tick(1000) 
@@ -72,7 +112,6 @@ def check_bound(rect, scr_rect):
     if rect.left < scr_rect.left or rect.right > scr_rect.right: yoko = -1
     if rect.top < scr_rect.top or rect.bottom > scr_rect.bottom: tate = -1
     return yoko,tate
-
 
 if __name__ == "__main__":
     pg.init()
