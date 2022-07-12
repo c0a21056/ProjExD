5e2c8325d175cfc3cf52e03811fa2ae5f1274383
kondo <c0a210568e@edu.teu.ac.jp>
2022-07-12 17:24:16 +0900

    4限基本機能実装完了

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 import random
+import tkinter.messagebox as tkm
 
 class Screen:
     def __init__(self, title, wh, image):
@@ -43,6 +44,7 @@ class Bird:
                 self.rct.centerx += 1
             if key_states[pg.K_RIGHT] :
                 self.rct.centerx -= 1
+        
         self.blit(scr)
 
 
@@ -67,7 +69,47 @@ class Bomb:
         self.vy *= tate
 
         self.blit(scr)
-        
+
+class Enemy:
+    def __init__(self, image: str, size: float, xy ,scr: Screen):
+        self.sfc = pg.image.load(image)                      # Surface
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  # Surface
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
+        self.vx, self.vy = xy                       
+    
+    def blit(self,scr: Screen):
+        scr.sfc.blit(self.sfc,self.rct)
+
+    
+    def update(self, scr :Screen):
+        self.rct.move_ip(self.vx, self.vy)
+
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+
+        self.blit(scr)
+
+
+class Beam:
+    def __init__(self, image: str, size: float, x, y, vx, scr:Screen ):
+        self.sfc = pg.image.load(image)                      # Surface
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, size)  # Surface
+        self.rct = self.sfc.get_rect()                       # Rect
+        self.rct.centerx = x
+        self.rct.centery = y
+        self.vx, self.vy = vx
+
+    def blit(self,scr: Screen):
+        scr.sfc.blit(self.sfc,self.rct)
+
+    def update(self, scr: Screen):
+        self.rct.move_ip(self.vx, self.vy) 
+
+        self.blit(scr)
+
 
 def main():
     clock = pg.time.Clock()
@@ -77,6 +119,10 @@ def main():
     tori = Bird("fig/6.png",2.0,(900, 400))
 
     bomb = Bomb((255,0,0), 10, (+1, +1), scr)
+
+    enemy = Enemy("fig/a.png",0.5, (+1,+1) ,scr)
+
+    beam = Beam("fig/beam.png",0.05, tori.rct.centerx,tori.rct.centery, (+1,+0) ,scr)
 
     while True:
         scr.blit()
@@ -88,7 +134,22 @@ def main():
 
         bomb.update(scr)
 
+        enemy.update(scr)
+
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_SPACE] :
+            beam.update(scr)
+
+        if beam.rct.colliderect(enemy.rct):
+            tkm.showinfo("ゲームクリア", "ゲームクリアです")
+            return
+
         if tori.rct.colliderect(bomb.rct):
+            tkm.showinfo("ゲームオーバー", "ゲームオーバーです")
+            return
+        
+        if tori.rct.colliderect(enemy.rct):
+            tkm.showinfo("ゲームオーバー", "ゲームオーバーです")
             return
 
         pg.display.update()
@@ -102,8 +163,10 @@ def check_bound(rct, scr_rct):
     [2] scr_rct: スクリーンのRect
     '''
     yoko, tate = +1, +1 # 領域内
-    if rct.left < scr_rct.left or scr_rct.right  < rct.right : yoko = -1 # 領域外
-    if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right :
+        yoko = -1 # 領域外
+    if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom:
+        tate = -1 # 領域外
     return yoko, tate
 
 
